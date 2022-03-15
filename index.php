64cbac959ce1748e0058d7a9b98adc297205e3d2
edwiner82 <edwiner_2006@hotmail.com>
2022-03-15 09:24:42 -0500

    agregue una tabla

--- index.php
+++ index.php
@@ -6,7 +6,11 @@ print "";
 print "</head>";
 print "<body>";
 print "<h1>Sitio de ayuda</h1>";
-print "";
+print "<table>";
+print "<tr><td colspam=2>Comando de git</td></tr>";
+print "<tr><td>Comando</td><td>Observación</td></tr>";
+print "<tr><td>git init</td><td>git</td></tr>";
+print "</table>";
 print "</body>";
 print "</html>";
 
